e40b19a9a924c9effed62d0d5d064f6f0b41097c
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2024-11-22 00:36:08 -0300

    finally project

---
+++ src/main/java/com/rocketseat/createUrlShortner/UrlData.java
@@ -0,0 +1,14 @@
+package com.rocketseat.createUrlShortner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
